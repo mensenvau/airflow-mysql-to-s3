4bb158fdaab7185e2a4c4f98445c6a611cea404b
Utkir Khujaev <balkibumen@gmail.com>
2025-03-09 02:43:10 +0500

    add .gitignore

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 A dynamic MySQL to S3 was created to migrate data from the Jobgram.org site to S3, which can also be used for other processes.
 
-Aifrlow (Init and Run):
+#### Aifrlow (Init and Run):
+
+[Please check this link for more details](https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html)
 
 ```bash
 docker compose up airflow-init
@@ -15,5 +17,14 @@ docker compose up -d
 S3 (minio):
 
 ```bash
-docker run -p 9000:9000 -p 9090:9090 -e "MINIO_ROOT_USER=admin" -e "MINIO_ROOT_PASSWORD=password" minio/minio:latest
+mkdir -p ~/minio/data
+
+docker run \
+   -p 9000:9000 \
+   -p 9001:9001 \
+   --name minio \
+   -v ~/minio/data:/data \
+   -e "MINIO_ROOT_USER=ROOTNAME" \
+   -e "MINIO_ROOT_PASSWORD=CHANGEME123" \
+   quay.io/minio/minio server /data --console-address ":9001"
 ```
--- dags/mysql_to_s3.py
+++ dags/mysql_to_s3.py
@@ -1,14 +1,23 @@
 # type: ignore
 
-from datetime import datetime, timedelta
+import logging as log
+import datetime as dt
 from airflow import DAG 
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2025, 1, 1),
-    'retry_delay': timedelta(minutes=5),
+    'start_date': dt.datetime(2025, 1, 1),
+    'retry_delay': dt.timedelta(minutes=5),
     'retries': 5
 }
+
+def get_dag_configs():
+    mysql_hook = MySqlHook(mysql_conn_id="my_mysql")
+    records = mysql_hook.get_records("select * from jobgram_main.jobs_main")
+    log.info(f"Records: {records}")
+    return records
 
 dag = DAG(
     dag_id='mysql_to_s3',
@@ -16,3 +25,11 @@ dag = DAG(
     schedule_interval='@daily',
     catchup=False
 )
+
+task1 = PythonOperator(
+    task_id='get_dag_configs',
+    python_callable=get_dag_configs,
+    dag=dag
+)
+
+task1
